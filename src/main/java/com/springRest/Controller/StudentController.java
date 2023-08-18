52d059f1f2e76e4f3ac26b334959eab8508ab84c
Murali Krishna <mailstomuralikrishna@gmail.com>
2023-08-18 09:59:57 +0530

    basic apis created

--- src/main/java/com/springRest/Controller/StudentController.java
+++ src/main/java/com/springRest/Controller/StudentController.java
@@ -2,6 +2,8 @@ package com.springRest.Controller;
 
 import com.springRest.res.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,4 +14,17 @@ public class StudentController {
         return new Student("Murali","Krishna");
 
     }
+
+    @GetMapping("/student/{firstname}/{lastname}")
+    public Student pathStudent(
+            @PathVariable("firstname") String fname,
+            @PathVariable("lastname") String lname){
+    return new Student(fname,lname);
+    }
+
+@GetMapping("/studentparam")
+    public Student paramStudent (@RequestParam(name="firstname") String firstname,@RequestParam(name="lastname") String lastname){
+    return new Student(firstname,lastname);
+    }
+
 }
